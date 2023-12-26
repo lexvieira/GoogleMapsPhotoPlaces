6392779ab69b806f68efe84cb54b479d01dd9ed6
Alex Vieira <lexvieira.info@gmail.com>
2023-12-26 15:43:46 +0700

    Google Maps API Photos - New Apis

--- src/controllers/urlUnshortController.ts
+++ src/controllers/urlUnshortController.ts
@@ -1,44 +1,275 @@
 import express, { Request, Response } from 'express';
-import { geturl, GoogleMapsPhoto, GoogleMapsPlaceID } from "../services/urlServices";
+import { geturl, GetGoogleMapsPhotoURL, GetGoogleMapsPlaceIDByAutoComplete, GetGoogleMapsPlaceIDByText, GetPlaceNameByURL, RenderImageGoogleMaps, saveFileFromJSON, RemoveKeyURL, DateDiffInDays, GetGoogleMapsPlaceDetails, GetGoogleMapsPhotoURLByLocalData } from "../services/urlServices";
 import * as bodyParser from 'body-parser';
 import axios from 'axios';
+import path from 'path';
+import { Console } from 'console';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const unshorterurl = express.Router();
-
-// unshorterurl.use(bodyParser.json());
-// unshorterurl.use(bodyParser.urlencoded({ extended: false }));
-
+const REMOVE_API_KEYS_FROM_LINKS = (process.env.REMOVE_API_KEYS_FROM_LINKS === 'true');
+const debugSteps = (process.env.DEBUG_STEPS === 'true');
 unshorterurl.use(express.json());
 
-unshorterurl.post('/get-url-picture', async (req: Request, res: Response) => {
-
+unshorterurl.post('/unshort-url', async (req: Request, res: Response) => {
+    const errorresponseData = {
+        "message": "It wasn't possible to Unshort the URL",
+        "errorMessage": "Request failed with status code 404"
+    };
     try {
         const { urllink } = req.body;
-        console.log(req.body.urllink)
-        console.log("Getting Body: " + urllink);
-        console.log("Getting Body: " + req.body);
-        // let urllink = "https://maps.app.goo.gl/wxkcrGqNXfpFwvk79"
         let url = urllink;
 
-        console.log(urllink);
-        try {
-            url = await geturl(urllink)
-        } catch (error) {
-            url = "Error Retrieving URL";
-        }
+        url = await geturl(urllink)
 
-        // console.log(url);
         const responseData = {
             "message": "Return URL:",
             "url": url
         };
         res.status(200).json(responseData)
-        // res.json(responseData);
     } catch (error) {
-        console.log(error);
-        throw error;
+        res.status(500).json(errorresponseData)
     }
+});
 
+unshorterurl.post('/unshort-url-array', async (req: Request, res: Response) => {
+    const errorresponseData = {
+        "message": "It wasn't possible to Unshort the URL",
+        "errorMessage": "Request failed with status code 404"
+    };
+
+    try {
+        const { urllink } = req.body;
+        let urls: [] = urllink;
+        let linksArray: ({})[] = [];
+        await Promise.all(urls.map(async (url, index) => {
+            let urlUnshortObj = {
+                id: index,
+                status: true,
+                urlUnshorted: "",
+                placeName: "",
+                placeID: "",
+                urlImage: "",
+                urlImageMaps: "",
+                storageDate: "",
+                expireDate: "",
+            };
+
+            // Request Unshort URL
+            // console.log(urlUnshortObj);
+            try {
+                let urlUnshort = await geturl(url);
+                urlUnshortObj.urlUnshorted = urlUnshort;
+                // Get the Local Name
+                urlUnshortObj.placeName = GetPlaceNameByURL(urlUnshort);
+
+                // Get Place ID
+                if (urlUnshortObj.placeName != "") {
+                    let placeID: any = await GetGoogleMapsPlaceIDByAutoComplete(urlUnshortObj.placeName)
+
+                    if (placeID === null) {
+                        placeID = await GetGoogleMapsPlaceIDByText(urlUnshortObj.placeName)
+                    }
+
+                    urlUnshortObj.placeID = placeID
+                    // Return URL Image
+                    let urlImage: any = await GetGoogleMapsPhotoURL(placeID)
+                    urlUnshortObj.urlImage = REMOVE_API_KEYS_FROM_LINKS ? RemoveKeyURL(urlImage) : urlImage
+                    // // Store the Link from lh3.googleusercontent.com/places 
+                    let urlImageMaps: any = await geturl(urlImage);
+                    urlUnshortObj.urlImageMaps = urlImageMaps
+                    // Set Storage and Expire Dates
+                    let storageDate: Date = new Date();
+                    storageDate.setHours(0, 0, 0, 0);
+                    let expireDate: Date = new Date(storageDate.setMonth(storageDate.getMonth() + 1));
+
+                    urlUnshortObj.storageDate = storageDate.toString();
+                    urlUnshortObj.expireDate = expireDate.toString();
+
+                    // Save Image - Not Necessary
+                    // RenderImageGoogleMaps(placeID, index, urlImage)
+                }
+            } catch (error) {
+                urlUnshortObj.urlUnshorted = "";
+                urlUnshortObj.status = false;
+                // console.log(error);
+            }
+
+            // console.log(urlUnshortObj);
+            linksArray.push(urlUnshortObj);
+            // console.log(linksArray);
+        }));
+
+        const responseData = {
+            "message": "Return URL:",
+            "urls": linksArray
+        };
+
+        res.status(200).json(responseData)
+    } catch (error) {
+        res.status(500).json(errorresponseData)
+    }
+});
+
+unshorterurl.post('/get-maps-url-array-photo', async (req: Request, res: Response) => {
+    const errorresponseData = {
+        "message": "It wasn't possible to Unshort the URL",
+        "errorMessage": "Request failed with status code 404"
+    };
+
+    let date: Date = new Date()
+    let year: number = date.getFullYear();
+    let month: number = date.getMonth();
+    let day: number = date.getDay();
+
+    let destinationPath: string = path.join(process.cwd(), '/src/data/', `data_${year}${month}${day}.json`);
+
+    try {
+        const { dataCountries, saveImages } = req.body;
+        let urls: [] = dataCountries;
+        let linksArray: any[] = [];
+
+        // console.log(urls);
+        await Promise.all(
+            urls.map(async (countries: any, index) => {
+            console.log(`Country: ${countries.country}`);
+            let dataCountry: any[] = countries.data;
+            // console.log(`Data Country: ${dataCountry}`);
+            await Promise.all(dataCountry.map(async (regions, index) => {
+                console.log(regions.region);
+                let dataRegion: any[] = regions.data;
+                await Promise.all(dataRegion.map(async (places, index) => {
+                    let urlUnshortObj = {
+                        id: index,
+                        status: true,
+                        urlUnshorted: "",
+                        placeDetails: {},
+                        placeName: "",
+                        placeID: "",
+                        urlImage: "",
+                        urlImageMaps: "",
+                        storageDate: "",
+                        expireDate: "",
+                    };
+
+                    // Request Unshort URL
+                    const currentDate: Date = new Date();
+                    const expireDate: Date = new Date(places.fulllink.expireDate);
+
+                    try {
+                        if (places.fulllink.placeName === "" || (!places.fulllink.expireDate || !expireDate || (currentDate > expireDate))) {
+                            if (debugSteps) {
+                                console.log("Checking Data")
+                            }
+                            let urlUnshort = await geturl(places.link)
+                            urlUnshortObj.urlUnshorted = urlUnshort
+                            if (debugSteps) {
+                                console.log(`Full URL: ${urlUnshort}`);
+                            }
+                            // Get the Local Name
+                            let placeName: string = GetPlaceNameByURL(urlUnshort)
+                            urlUnshortObj.placeName = placeName;
+
+                            if (debugSteps) {
+                                console.log(`Place Name: ${urlUnshortObj.placeName}`);
+                            }
+                            // Get Place ID
+                            if (urlUnshortObj.placeName != "") {
+                                // Get Place ID from Google Maps API
+                                let placeID: any = await GetGoogleMapsPlaceIDByAutoComplete(urlUnshortObj.placeName)
+                                if (debugSteps) {
+                                    console.log(`First Try: ${placeID}`)
+                                }
+                                if (placeID === null) {
+                                    placeID = await GetGoogleMapsPlaceIDByText(urlUnshortObj.placeName)
+                                }
+                                if (debugSteps) {
+                                    console.log(`Second Try: ${placeID}`)
+                                }
+                                urlUnshortObj.placeID = placeID
+
+                                if (debugSteps) {
+                                    console.log(`Place ID: ${urlUnshortObj.placeID}`);
+                                }
+                                // Get Details Based on Google Maps PlaceID // Useful to Render a Gallery on the next Stages.
+                                let placeDetails: any = await GetGoogleMapsPlaceDetails(placeID)
+
+                                // saveFileFromJSON(placeDetails,destinationPath)
+
+                                if (debugSteps) {
+                                    // console.log(placeDetails);
+                                }
+                                urlUnshortObj.placeDetails = placeDetails
+                                // Return URL Image
+                                if (placeDetails.status === 'OK') {
+                                    // && placeDetails && placeDetails.length > 0
+                                    console.log(`Debug Steps: place Details: ${debugSteps}`)
+                                    if (debugSteps) {
+                                        console.log("Saving place Details");
+                                    }
+                                    let urlImage: any = await GetGoogleMapsPhotoURLByLocalData(placeDetails);
+                                    // let urlImage: any = await GetGoogleMapsPhotoURL(placeID) //Request Just The URL Image
+                                    urlUnshortObj.urlImage = REMOVE_API_KEYS_FROM_LINKS ? RemoveKeyURL(urlImage) : urlImage
+                                    // Store the Link from lh3.googleusercontent.com/places 
+                                    let urlImageMaps: any = await geturl(urlImage);
+                                    urlUnshortObj.urlImageMaps = urlImageMaps
+
+                                    places.image = urlImageMaps;
+                                    // Set Storage and Expire Dates
+                                    let storageDate: Date = new Date();
+                                    storageDate.setHours(0, 0, 0, 0);
+                                    let expireDate: Date = new Date(storageDate.setMonth(storageDate.getMonth() + 1));
+
+                                    urlUnshortObj.storageDate = storageDate.toString();
+                                    urlUnshortObj.expireDate = expireDate.toString();
+                                }
+
+                                // Save Image - Not Necessary
+                                // RenderImageGoogleMaps(placeID, index, urlImage)
+                            }
+                        }
+
+                    } catch (error) {
+                        urlUnshortObj.urlUnshorted = ""
+                        urlUnshortObj.status = false
+                        if (debugSteps) {
+                            console.log(`Error: ${error}`);
+                        }
+                    }
+                    try {
+                        if (debugSteps) {
+                            console.log("Filling Fullink")
+                        }
+                        if (debugSteps) {
+                            console.log(urlUnshortObj);
+                        }
+                        places.fulllink = { ...urlUnshortObj }
+                    } catch (error) {
+                        if (debugSteps) {
+                            console.log("Fail Add Object")
+                        }
+                    }
+                    if (debugSteps) {
+                        console.log(`Places: `)
+                        console.log(places)
+                    }
+                }));
+            }));
+        }));
+
+        saveFileFromJSON(urls, destinationPath);
+
+        const responseData = {
+            "message": "Return URL:",
+            "urls": urls
+        };
+
+        res.status(200).json(responseData)
+    } catch (error) {
+        res.status(500).json(errorresponseData)
+    }
 });
 
 unshorterurl.get('/get-photo/:id', async (req: Request, res: Response) => {
@@ -46,9 +277,9 @@ unshorterurl.get('/get-photo/:id', async (req: Request, res: Response) => {
     var id = req.params.id;
     console.log(id);
 
-    let placeID = await GoogleMapsPlaceID(id)
+    let placeID = await GetGoogleMapsPlaceIDByAutoComplete(id)
 
-    let photoURL = await GoogleMapsPhoto(placeID)
+    let photoURL = await GetGoogleMapsPhotoURL(placeID)
     const responseData = {
         "message": "Return URL:",
         "placeid": placeID,
--- src/services/urlServices.ts
+++ src/services/urlServices.ts
@@ -1,87 +1,286 @@
-import * as bodyParser from 'body-parser';
 import axios from 'axios';
+import { response } from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const GOOGLEAPIKEY = process.env.MAPS_API_KEY
+const debugSteps = (process.env.DEBUG_STEPS === 'true');
 
 export async function geturl(link: string) {
+  var url = "1";
+  try {
+    await axios({
+      method: "get",
+      url: link,
+      maxRedirects: 0
+    }).then(res => {
+      // url = "2";
+      // console.log(url);
+      url = res.data.args;
+      // console.log(url);
+    });
+  } catch (error: any) {
+    // console.log(error.response.headers.location);
+    // throw error;
+    if (Math.trunc(error.response.status / 100) === 3) {
 
-    var url = "1";
-    try {
-        await axios({
-            method: "get",
-            url: link,
-            maxRedirects: 0
-        }).then(res => {
-            // url = "2";
-            // console.log(url);
-            url = res.data.args;
-            // console.log(url);
-        });
-
-    } catch (error: any) {
-        if (Math.trunc(error.response.status / 100) === 3) {
-
-            url = error.response.headers.location;// 
-            // return url; //geturl(error.response.headers.location);
-        } else {
-            url = "It wasn't possible to Unshort the URL";
-        }
+      url = error.response.headers.location;// 
+      // return url; //geturl(error.response.headers.location);
+    } else {
+      // console.log(error);
+      throw error;
+      // url = "It wasn't possible to Unshort the URL";
     }
-    console.log(url);
-    return url;
+  }
+  // console.log(url);
+  return url;
 }
 
-export const GoogleMapsPhoto = async (placeID: any) => {
+export const GetGoogleMapsPhotoURLByLocalData = async (localData: any) => {
+  if (localData.status === 'OK' && localData.result.photos && localData.result.photos.length > 0) {
+    const photoReference = localData.result.photos[0].photo_reference;
+    if (debugSteps) {
+      console.log(`Checking Photo References: ${photoReference}`);
+    }
+    const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${photoReference}&key=${GOOGLEAPIKEY}`;
+    // console.log('Photo URL:', photoURL);
+    return photoURL;
+  } else {
+    console.log('No photos found for this place.');
+    return null;
+  }
+}
+
+export const GetGoogleMapsPhotoURL = async (placeID: any) => {
+  try {
+    // const GOOGLEAPIKEY = process.env.MAPS_API_KEY
+    // let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&key=${GOOGLEAPIKEY}`  
+    // console.log(fetchURLPlace)
     try {
-        const GOOGLEAPIKEY = process.env.MAPS_API_KEY
-        // let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&key=${GOOGLEAPIKEY}`  
-        // console.log(fetchURLPlace)
-        // console.log(fetchURLPlace)
+      let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=photos&key=${GOOGLEAPIKEY}`
 
-          try {
-            let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=photos&key=${GOOGLEAPIKEY}`  
+      const response = await axios.get(fetchURLPlace);
 
-            const response = await axios.get(fetchURLPlace);
-            
-            if (response.data.status === 'OK' && response.data.result.photos && response.data.result.photos.length > 0) {
-              const photoReference = response.data.result.photos[0].photo_reference;
-              const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${GOOGLEAPIKEY}`;
-              console.log('Photo URL:', photoURL);
-              return photoURL;
-            } else {
-              console.log('No photos found for this place.');
-              return null;
-            }
-          } catch (error) {
-            console.error('Error:', error);
-            return null;
-          }          
+      if (response.data.status === 'OK' && response.data.result.photos && response.data.result.photos.length > 0) {
+        const photoReference = response.data.result.photos[0].photo_reference;
+
+        const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${photoReference}&key=${GOOGLEAPIKEY}`;
+        // console.log('Photo URL:', photoURL);
+        return photoURL;
+      } else {
+        console.log('No photos found for this place.');
+        return null;
+      }
     } catch (error) {
-        console.error(`Error fetching image for ${placeID}: ${error}`);
+      console.error('Error:', error);
+      return null;
     }
+  } catch (error) {
+    console.error(`Error fetching image for ${placeID}: ${error}`);
+  }
 }
 
-export const GoogleMapsPlaceID = async (placeName: any) => {
-    try {
-        const GOOGLEAPIKEY = process.env.MAPS_API_KEY
-        // let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&key=${GOOGLEAPIKEY}`  
-        // console.log(fetchURLPlace)
-        let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${placeName}&key=${GOOGLEAPIKEY}`  
-        // console.log(fetchURLPlace)
-        try {
-            const response = await axios.get(fetchURLPlace);
-            if (response.data.status === 'OK' && response.data.predictions.length > 0) {
-              const placeID = response.data.predictions[0].place_id;
-              console.log('Place ID:', placeID);
-              return placeID;
-            } else {
-              console.log('No results found.');
-              return "Not Found";
-            }
-          } catch (error) {
-            console.error('Error:', error);
-            return "Error Searching";
-          }
-    } catch (error) {
-        console.error(`Error fetching image for ${placeName}: ${error}`);
+export const GetGoogleMapsPlaceDetails = async (placeID: any) => {
+  try {
+    let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=photos&key=${GOOGLEAPIKEY}`
+    // console.log(fetchURLPlace)
+    // return;
+    // const response:any = await axios.get(fetchURLPlace);
+
+    const response = await axios.get(fetchURLPlace);
+
+    if (response.data.status === 'OK') {
+      // console.log(response);
+      let placeDetails;
+      placeDetails = response.data;
+
+      // const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${photoReference}&key=${GOOGLEAPIKEY}`;
+      // console.log('Photo URL:', photoURL);
+      return placeDetails;
+    } else {
+      console.log('No Details found for this place.');
+      return null;
     }
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
 }
 
+export const GetGoogleMapsPlaceIDByText = async (placeName: any) => {
+  let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&key=${GOOGLEAPIKEY}`
+  try {
+    const response = await axios.get(fetchURLPlace);
+    if (response.data.status === 'OK' && response.data.candidates.length > 0) {
+      const placeID = response.data.candidates[0].place_id;
+      if (debugSteps) {
+        console.log('Place ID (Find Place From Text):', placeID);
+      }
+      return placeID;
+    } else {
+      // console.log('No results found.');
+      return null;
+    }
+  } catch (error) {
+    if (debugSteps) {
+      console.log(`Place ID Not Found for Place Name: ${placeName}: ${error}`);
+    }
+    // console.error('Error:', error);
+    return null;
+  }
+}
+
+export const GetGoogleMapsPlaceIDByAutoComplete = async (placeName: any) => {
+  let fetchURLPlace = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${placeName}&key=${GOOGLEAPIKEY}`
+  try {
+    const response = await axios.get(fetchURLPlace);
+    if (response.data.status === 'OK' && response.data.predictions.length > 0) {
+      const placeID = response.data.predictions[0].place_id;
+      if (debugSteps) {
+        console.log('Place ID (AutoComplete):', placeID);
+      }
+      return placeID;
+    } else {
+      // console.log('No results found.');
+      return null;
+    }
+  } catch (error) {
+    // console.error('Error:', error);
+    console.error(`Place ID Not Found for Place Name: ${placeName}: ${error}`);
+    return null;
+  }
+}
+
+export const GetPlaceNameByURL = (placeURL: string) => {
+  let placeName = ""
+  try {
+    placeName = placeURL.split("place/")[1].split("/")[0];
+  } catch (error) {
+    placeName = ""
+  }
+  return placeName;
+}
+
+export const RenderImageGoogleMaps = (placeID: string, idURL: number, urlImage: string) => {
+  let imageName: string = "img_" + idURL + "_" + placeID + ".jpg"
+  // const file = fs.createWriteStream(imageName);
+
+  let destinationPath: string = path.join(process.cwd(), '/src/images/', imageName);
+
+  // Save Image on Disk
+  // https.get(urlImage, response => {
+  //   response.pipe(file);
+  //   file.on('finish', () => {
+  //     file.close();
+  //     console.log(`Image downloaded as ${imageName}`);
+  //   });
+
+  //   // Creating symlink to file 
+  //   fs.symlinkSync(path.join(process.cwd(), '/src/images/', imageName) , "symlinkToFile");
+  //   // fs.symlinkSync(__dirname + "\\" + "images\\" + imageName, "symlinkToFile");
+
+  //   console.log("\nImage was Created"); 
+
+  //   }).on('error', err => {
+  //       fs.unlink(imageName, (err => {
+  //         if (err) console.log(err); 
+  //         else { 
+  //           console.log("\nDeleted symlinkToFile"); 
+
+  //           // Get the files in current directory 
+  //           // after deletion 
+  //           getFilesInDirectory(); 
+  //         } 
+  //       }));
+  //   })
+
+  saveFileFromURL(urlImage, destinationPath)
+    .then(() => {
+      console.log('Image downloaded and saved successfully.');
+      // Further processing or database storage can be done here
+    })
+    .catch((err) => {
+      console.error('Failed to download and save the image:', err);
+    });
+
+}
+
+
+// Function to download and save an image
+const saveFileFromURL = async (fileURL: string, destinationPath: string) => {
+  try {
+    const response = await axios({
+      url: fileURL,
+      method: 'GET',
+      responseType: 'stream',
+    });
+
+    // Create a writable stream and pipe the image data to the file
+    const writer = fs.createWriteStream(destinationPath);
+    response.data.pipe(writer);
+
+    // Return a promise to handle the completion or errors
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+  } catch (error) {
+    console.error('Error downloading image:', error);
+    throw new Error('Error downloading image');
+  }
+};
+
+export const saveFileFromJSON = async (file: [], destinationPath: string) => {
+  try {
+
+    const jsonData = JSON.stringify(file, null, 2);
+
+    fs.writeFile(destinationPath, jsonData, (err) => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+      } else {
+        console.log('JSON file has been saved successfully.');
+      }
+    });
+
+  } catch (error) {
+    console.error('Error Saving File:', error);
+    // throw new Error('Error Saving File');
+  }
+};
+
+// Function to get current filenames 
+// in directory with specific extension 
+const getFilesInDirectory = () => {
+  console.log("\nFiles present in directory:");
+  let files = fs.readdirSync(__dirname);
+  files.forEach(file => {
+    console.log(file);
+  });
+}
+
+export const RemoveKeyURL = (url: string) => {
+  return url.split("&key")[0]
+}
+
+export const DateDiffInDays = (dateStart: string, dateEnd: string) => {
+  if (!dateStart || !dateEnd) {
+    return false;
+  }
+
+  const date1: Date = new Date(dateStart);
+  const date2: Date = new Date(dateEnd);
+
+  if (isNaN(date1.getTime()) || isNaN(date2.getTime())) {
+    return false;
+  }
+
+  const differenceInMs = Math.abs(date2.getTime() - date1.getTime());
+  const differenceInDays = Math.ceil(differenceInMs / (1000 * 60 * 60 * 24));
+
+  return differenceInDays;
+}
